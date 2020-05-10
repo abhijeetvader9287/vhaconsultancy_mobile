ae6b82d16ad12f2d4cf036ca4d25a5620eba988a
Abhijeet Vader <abhijeetvader@gmail.com>
2020-05-10 15:45:05 +0530

    ferlizer and work status updated

--- app/src/main/java/com/smile/vhaconsultancy/adapters/PruningListRecyclerViewAdapter.kt
+++ app/src/main/java/com/smile/vhaconsultancy/adapters/PruningListRecyclerViewAdapter.kt
@@ -6,14 +6,28 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.smile.vhaconsultancy.R
 import com.smile.vhaconsultancy.models.AprilPruningModel
+import com.smile.vhaconsultancy.utilities.SharedPref
 import java.util.*
 
 class PruningListRecyclerViewAdapter(private val plots: ArrayList<AprilPruningModel>) : RecyclerView.Adapter<PruningListRecyclerViewAdapter.ViewHolder>() {
+    var userPhoneNumber: String? = ""
+    var database: FirebaseDatabase? = null
+    var plot_key: String? = ""
+    var databasePlotListReference: DatabaseReference? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.pruning_item, parent, false)
+        database = FirebaseDatabase.getInstance()
+
+        userPhoneNumber = SharedPref.Companion.getInstance(parent.context)?.getSharedPref(parent.context.getString(R.string.userPhoneNumber))
+        plot_key=SharedPref.Companion.getInstance(parent.context)?.getSharedPref(parent.context.getString(R.string.plot_key))
+
+        databasePlotListReference = database!!.getReference(parent.context.getString(R.string.user_list)).child(userPhoneNumber!!).child(parent.context.getString(R.string.plot_list)).child(plot_key.toString()).child("april_pruning_list")
+
         return ViewHolder(view)
     }
 
@@ -25,6 +39,18 @@ class PruningListRecyclerViewAdapter(private val plots: ArrayList<AprilPruningMo
         holder.txtNumerOfVine.text = plot.fertilizer.toString() + ""
         holder.checkboxFertilizer.isChecked=plot.fertilizer_completed
         holder.checkboxWork.isChecked=plot.work_spray_completed
+        holder.checkboxWork.setOnCheckedChangeListener { buttonView, isChecked ->
+
+                databasePlotListReference?.child(plot.srNo.toString())?.child("work_spray_completed")?.setValue(isChecked)
+                //Do Whatever you want in isChecked
+
+        }
+        holder.checkboxFertilizer.setOnCheckedChangeListener { buttonView, isChecked ->
+
+                databasePlotListReference?.child(plot.srNo.toString())?.child("fertilizer_completed")?.setValue(isChecked)
+                //Do Whatever you want in isChecked
+
+        }
     }
 
     override fun getItemCount(): Int {
