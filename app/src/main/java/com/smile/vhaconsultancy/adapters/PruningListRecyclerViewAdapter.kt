d248b9066089a46060f7befeb7f2121ff57228bb
Abhijeet Vader <abhijeetvader@gmail.com>
2020-05-12 12:19:56 +0530

    checkbox replaced by buttons

--- app/src/main/java/com/smile/vhaconsultancy/adapters/PruningListRecyclerViewAdapter.kt
+++ app/src/main/java/com/smile/vhaconsultancy/adapters/PruningListRecyclerViewAdapter.kt
@@ -1,9 +1,10 @@
 package com.smile.vhaconsultancy.adapters
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DatabaseReference
@@ -37,21 +38,55 @@ class PruningListRecyclerViewAdapter(private val plots: ArrayList<AprilPruningMo
         holder.txtVariety.text = plots[position].strDate + ""
         holder.txtDistance.text = plots[position].work_spray.toString() + ""
         holder.txtNumerOfVine.text = plots[position].fertilizer.toString() + ""
-        holder.checkboxFertilizer.isChecked=plots[position].fertilizer_completed
-        holder.checkboxWork.isChecked=plots[position].work_spray_completed
-        holder.checkboxWork.setOnCheckedChangeListener { buttonView, isChecked ->
+     //   holder.checkboxFertilizer.isEnabled=!plots[position].fertilizer_completed
+       // holder.checkboxWork.isEnabled=!plots[position].work_spray_completed
+        if(plots[position].fertilizer_completed)
+        {
+            holder.checkboxFertilizer.setPaintFlags(holder.checkboxFertilizer.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG)
 
-               databasePlotListReference?.child(plots[position].srNo.toString())?.child("work_spray_completed")?.setValue(isChecked)
-            plots[position].work_spray_completed=isChecked
+        //    holder.checkboxFertilizer.setBackgroundColor(holder.checkboxFertilizer.context.resources.getColor(R.color.add_amount_grey))
+        }
+        else
+        {
+            holder.checkboxFertilizer.setPaintFlags(holder.checkboxFertilizer.getPaintFlags() and Paint.STRIKE_THRU_TEXT_FLAG.inv())
+
+        }
+
+        if(plots[position].work_spray_completed)
+        {
+            holder.checkboxWork.setPaintFlags(holder.checkboxWork.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG)
+
+            //    holder.checkboxFertilizer.setBackgroundColor(holder.checkboxFertilizer.context.resources.getColor(R.color.add_amount_grey))
+        }
+        else
+        {
+            holder.checkboxWork.setPaintFlags(holder.checkboxWork.getPaintFlags() and Paint.STRIKE_THRU_TEXT_FLAG.inv())
+
+        }
+        holder.checkboxWork.setOnClickListener { buttonView  ->
+            if( !plots[position].work_spray_completed) {
+                databasePlotListReference?.child(plots[position].srNo.toString())?.child("work_spray_completed")?.setValue(true)
+                plots[position].work_spray_completed=true
+            }else{
+                databasePlotListReference?.child(plots[position].srNo.toString())?.child("work_spray_completed")?.setValue(false)
+                plots[position].work_spray_completed=false
+            }
+
+
          //   notifyItemChanged(position)
                 //Do Whatever you want in isChecked
 
         }
-        holder.checkboxFertilizer.setOnCheckedChangeListener { buttonView, isChecked ->
-
-                databasePlotListReference?.child(plots[position].srNo.toString())?.child("fertilizer_completed")?.setValue(isChecked)
-                //Do Whatever you want in isChecked
-            plots[position].fertilizer_completed=isChecked
+        holder.checkboxFertilizer.setOnClickListener { buttonView  ->
+if(! plots[position].fertilizer_completed) {
+    databasePlotListReference?.child(plots[position].srNo.toString())?.child("fertilizer_completed")?.setValue(true)
+    //Do Whatever you want in isChecked
+    plots[position].fertilizer_completed = true
+}else{
+    databasePlotListReference?.child(plots[position].srNo.toString())?.child("fertilizer_completed")?.setValue(false)
+    //Do Whatever you want in isChecked
+    plots[position].fertilizer_completed = false
+}
 //            notifyItemChanged(position)
         }
     }
@@ -65,8 +100,8 @@ class PruningListRecyclerViewAdapter(private val plots: ArrayList<AprilPruningMo
         val txtVariety: TextView
         val txtDistance: TextView
         val txtNumerOfVine: TextView
-        val checkboxWork: CheckBox
-        val checkboxFertilizer: CheckBox
+        val checkboxWork: Button
+        val checkboxFertilizer: Button
 
         override fun toString(): String {
             return super.toString() + " '"
@@ -77,8 +112,8 @@ class PruningListRecyclerViewAdapter(private val plots: ArrayList<AprilPruningMo
             txtVariety = mView.findViewById<View>(R.id.txtVariety) as TextView
             txtDistance = mView.findViewById<View>(R.id.txtDistance) as TextView
             txtNumerOfVine = mView.findViewById<View>(R.id.txtNumerOfVine) as TextView
-            checkboxFertilizer = mView.findViewById<View>(R.id.checkboxFertilizer) as CheckBox
-            checkboxWork = mView.findViewById<View>(R.id.checkboxWork) as CheckBox
+            checkboxFertilizer = mView.findViewById<View>(R.id.checkboxFertilizer) as Button
+            checkboxWork = mView.findViewById<View>(R.id.checkboxWork) as Button
 
         }
     }
